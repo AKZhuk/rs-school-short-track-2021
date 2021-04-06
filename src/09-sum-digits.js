78c6fb510009d89f63589d96e24e4814bb710026
github-AKZhuk <lesha.zhuk@gmail.com>
2021-04-07 02:25:15 +0300

    feat: implement getSumOfDigits task

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -9,9 +9,13 @@
  * For 100, the result should be 1 (1 + 0 + 0 = 1)
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
- */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+*/
+function getSumOfDigits(n) {
+  let sum = n;
+  while (sum > 9) {
+    sum = (sum % 10) + (sum - (sum % 10)) / 10;
+  }
+  return sum;
 }
 
 module.exports = getSumOfDigits;
