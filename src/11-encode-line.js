e90485d99962feab58dfad75fb6be10d453dfc56
github-AKZhuk <lesha.zhuk@gmail.com>
2021-04-11 22:34:07 +0300

    feat: inplement encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -5,11 +5,24 @@
  * @return {String}
  *
  * @example
+ *
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let count = 1;
+    const letter = arr[i];
+    while (arr[i] === arr[i + 1]) {
+      count++;
+      i++;
+    }
+    if (count > 1) result.push(count);
+    result.push(letter);
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
